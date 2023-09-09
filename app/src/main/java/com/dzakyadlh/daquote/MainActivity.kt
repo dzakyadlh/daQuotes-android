2a0a47f6f67ea5ac6b7fb783faa00546eff19542
Dzaky Adla <106021129+dzakyadlh@users.noreply.github.com>
2023-09-10 00:06:25 +0700

    initial push

---
+++ app/src/main/java/com/dzakyadlh/daquote/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.dzakyadlh.daquote
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.dzakyadlh.daquote.databinding.ActivityMainBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
+import java.util.Random
+
+//"https://api.quotable.io/random"
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getRandomQuote()
+
+        val btnShare = findViewById<Button>(R.id.btnShare)
+
+        binding.btnNewQuote.setOnClickListener {
+            getRandomQuote()
+        }
+
+        binding.btnAllQuotes.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ListQuotesActivity::class.java))
+        }
+    }
+
+    private fun getRandomQuote() {
+        binding.progressBar.visibility = View.VISIBLE
+        val client = AsyncHttpClient()
+        val url = "https://api.quotable.io/random"
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>,
+                responseBody: ByteArray
+            ) {
+                binding.progressBar.visibility = View.INVISIBLE
+
+                val result = String(responseBody)
+                Log.d(TAG, result)
+                try {
+                    val responseObject = JSONObject(result)
+
+                    val quote = responseObject.getString("content")
+                    val author = responseObject.getString("author")
+
+                    randomColor()
+                    binding.tvQuote.text = quote
+                    binding.tvAuthor.text = author
+                } catch (e:Exception) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>,
+                responseBody: ByteArray,
+                error: Throwable
+            ) {
+                binding.progressBar.visibility = View.INVISIBLE
+
+                val errorMessage = when(statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error?.message}"
+                }
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    public fun randomColor(){
+        val containerBackground = findViewById<View>(R.id.activity_main)
+        val buttonNewQuoteColor = findViewById<Button>(R.id.btnNewQuote)
+        val buttonShareColor = findViewById<Button>(R.id.btnShare)
+        val buttonAllQuotesColor = findViewById<Button>(R.id.btnAllQuotes)
+        val quoteColor = findViewById<TextView>(R.id.tvQuote)
+        val authorColor = findViewById<TextView>(R.id.tvAuthor)
+        val rnd = Random()
+        val color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+        containerBackground.setBackgroundColor(color)
+        buttonNewQuoteColor.setBackgroundColor(color)
+        buttonShareColor.setBackgroundColor(color)
+        buttonAllQuotesColor.setBackgroundColor(color)
+        quoteColor.setTextColor(color)
+        authorColor.setTextColor(color)
+    }
+}
